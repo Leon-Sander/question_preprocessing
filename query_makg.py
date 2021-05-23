ebc6476ea7fbcc216ad7320bfa30dcc32918d112
Leon Sander <leon.sander@protonmail.com>
2021-05-23 14:25:39 +0200

    first steps baseline

---
+++ query_makg.py
@@ -0,0 +1,64 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+import pandas as pd
+
+class mag_connection:
+
+    def __init__(self):
+        sparql_endpoint = 'https://makg.org/sparql'
+        self.sparql = SPARQLWrapper(sparql_endpoint)
+        self.sparql.setReturnFormat(JSON)
+
+    def query_makg(self, author, keyword):
+        paper_list = []
+        if (author != None) and (keyword != None):
+            self.sparql.setQuery("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+                PREFIX dcterms: <http://purl.org/dc/terms/>
+                PREFIX magc: <https://makg.org/class/>
+                PREFIX prism: <http://prismstandard.org/namespaces/basic/2.0/>
+                SELECT ?paper
+                WHERE {
+                    
+                    ?author rdf:type <https://makg.org/class/Author> .
+                    ?author foaf:name """ + '\"' + author + '\"' + """^^xsd:string .
+                    ?paper dcterms:creator ?author .
+                    ?paper rdf:type magc:Paper .
+                    ?paper prism:keyword """ + '\"' + keyword + '\"' + """^^xsd:string .
+                }
+                """)
+            
+
+
+        elif (author == None) and (keyword != None):
+            self.sparql.setQuery("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                PREFIX magc: <https://makg.org/class/>
+                PREFIX prism: <http://prismstandard.org/namespaces/basic/2.0/>
+                SELECT ?paper
+                WHERE {
+                    ?paper rdf:type magc:Paper .
+                    ?paper prism:keyword """ + '\"' + keyword + '\"' + """^^xsd:string .
+                }
+                """)
+
+
+
+        elif (author != None) and (keyword == None):
+            self.sparql.setQuery("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+                PREFIX dcterms: <http://purl.org/dc/terms/>
+                PREFIX magc: <https://makg.org/class/>
+                SELECT ?paper
+                WHERE {
+                    
+                    ?author rdf:type <https://makg.org/class/Author> .
+                    ?author foaf:name """ + '\"' + author + '\"' + """^^xsd:string .
+                    ?paper dcterms:creator ?author .
+                }
+                """)
+        
+        results = self.sparql.query().convert()
+        results = results['results']['bindings']
+        if results != []:
+            for paper in results:
+                paper_list.append(paper['paper']['value'])
+            return paper_list
---
+++ question_preprocessing.py
@@ -0,0 +1,95 @@
+from nltk import pos_tag, word_tokenize
+import re
+import json
+from query_makg import mag_connection
+
+class question_preprocessing:
+    
+
+    def __init__(self):
+        self.qm = mag_connection()
+        pass
+
+    def question_classification(self,input_question):
+        plural = self.check_plural(input_question)
+        
+        brackets = self.check_brackets(input_question)
+        if brackets != None:
+            author , keyword = self.extract_author_and_keyword(input_question[brackets[0]+1:brackets[1]-1])
+            if (author == None) and (keyword == None):
+                print('Error, could not find an author or a keyword to query the mag')
+                return None
+    
+            paper_id = self.query_makg(author, keyword)
+
+
+        kg = self.template_matching(input_question)
+
+
+        if kg == False:
+            output = json.dumps({'plural': plural, 
+                                'MAG-IDs/MAKG-IDs' : paper_id,
+                                'KG' : kg})
+        else:
+            annotations = ''
+            imrad = ''
+            output = json.dumps({'plural': plural, 
+                                'MAG-IDs/MAKG-IDs' : paper_id,
+                                'KG' : kg,
+                                'annoations' : annoations,
+                                'imrad' : imrad})
+        return output
+    
+    def check_plural(self,input_question):
+        """This Method checks if the first Noun is singular or plural
+
+
+        :param input_question: input sentence to check on 
+        """
+        pos_tags = pos_tag(word_tokenize(input_question))
+        for tag in pos_tags:
+            if (tag[1] == 'NNS') or (tag[1] == 'NNPS'):
+                
+                return True
+            else:
+                if (tag[1] == 'NN') or (tag[1] == 'NNP'):
+                    
+                    return False
+                
+        print('No noun found.')
+        return False
+
+    def check_brackets(self, input_question):
+        brackets = re.search(r"\[.*\]",input_question)
+        if brackets != None:
+            return brackets.span()
+        else:
+            return None
+
+    def extract_author_and_keyword(self,input):
+        keywords_for_authors = "written|authored" 
+        result = re.search("(" + keywords_for_authors + ") by",input)
+        if result != None:
+            # gibt das erste Wort nach dem match aus, muss noch erweitert werden für längere Namen
+            author = input[result.span()[1]:].strip().split(" ")[0]
+
+        keywords_for_keyword = "about|on|regarding|with respect to"
+        result = re.search("(paper|papers) .* (" + keywords_for_keyword + ") ",input)
+        if result != None:
+            # hier ebenso nur das erste Wort nach dem match
+            keyword = input[result.span()[1]:].strip().split(" ")[0]
+
+
+        return author , keyword
+
+    def query_makg(self, author, keyword):
+        # queried nach dem exakten author und dem exakten keyword
+        return self.qm.query_makg(author, keyword)
+
+    def template_matching(self, input_question):
+        templates =  ["Which papers use .*?", "In which year was the paper .* published?", "Which papers are associated with.*?"]
+        for template in templates:
+            if re.match(template, input_question):
+                return True
+
+        return False
